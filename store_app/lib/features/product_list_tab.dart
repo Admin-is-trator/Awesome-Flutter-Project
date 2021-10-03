2157d160d4b89fcb4c47d6fb6969c1bf4eac6af7
hafizsiregar <hafizmessi81@gmail.com>
2021-10-03 21:28:49 +0700

    Menambahkan Produk

--- store_app/lib/features/product_list_tab.dart
+++ store_app/lib/features/product_list_tab.dart
@@ -2,21 +2,42 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:store_app/provider/app_state_model.dart';
 
-class ProductListTabPage extends StatelessWidget {
-  const ProductListTabPage({Key? key}) : super(key: key);
+import 'product_item_row.dart';
 
+class ProductListTabPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppStateModel>(
-      builder: (context, model, child) {
-        return const CustomScrollView(
-          slivers: <Widget>[
-            CupertinoSliverNavigationBar(
-              largeTitle: Text('IDN Store'),
-            ),
-          ],
-        );
-      },
+    return CupertinoPageScaffold(
+      child: Consumer<AppStateModel>(
+        builder: (context, model, child) {
+          final products = model.getProducts();
+          return CustomScrollView(
+            semanticChildCount: products.length,
+            slivers: <Widget>[
+              CupertinoSliverNavigationBar(
+                largeTitle: Text('IDN Store'),
+              ),
+              SliverSafeArea(
+                top: false,
+                minimum: const EdgeInsets.only(top: 8),
+                sliver: SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      if (index < products.length) {
+                        return ProductRowItem(
+                          product: products[index],
+                          lastItem: index == products.length - 1,
+                        );
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
